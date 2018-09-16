798b85a32c0270fe78d21e00684ebb93eb6d663b
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2018-09-16 04:33:10 +0300

    Improve documentation of experimental annotations

--- libraries/stdlib/src/kotlin/annotations/Inference.kt
+++ libraries/stdlib/src/kotlin/annotations/Inference.kt
@@ -9,7 +9,23 @@ import kotlin.annotation.AnnotationTarget.*
 import kotlin.experimental.ExperimentalTypeInference
 
 /**
- * Marks the API which usages is dependent on the experimental builder inference.
+ * Allows to infer generic type arguments of a function from the calls in the annotated function parameter of that function.
+ *
+ * When this annotation is placed on a generic function parameter of a function,
+ * it enables to infer the type arguments of that generic function from the lambda body passed to that parameter.
+ *
+ * Example: we declare
+ * ```
+ * fun <T> sequence(@BuilderInference block: suspend SequenceScope<T>.() -> Unit): Sequence<T>
+ * ```
+ * and use it like
+ * ```
+ * val result = sequence { yield("result") }
+ * ```
+ * Here the type argument of the resulting sequence is inferred to `String` from
+ * the argument of the [SequenceScope.yield] function, that is called inside the lambda passed to [sequence].
+ *
+ * Note: this annotation is experimental, see [ExperimentalTypeInference] on how to opt-in for it.
  */
 @Target(VALUE_PARAMETER, FUNCTION, PROPERTY)
 @Retention(AnnotationRetention.BINARY)
