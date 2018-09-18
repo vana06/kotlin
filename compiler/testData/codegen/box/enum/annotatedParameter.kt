1f63289935be1f73c3a4949cd3a4a92f95abde79
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2018-09-18 10:32:21 +0200

    Generate private constructors for Enums
      #KT-2680 Fixed
      #KT-16867 Fixed

---
+++ compiler/testData/codegen/box/enum/annotatedParameter.kt
@@ -0,0 +1,22 @@
+// FILE: Foo.java
+
+public class Foo {
+
+    static String test() {
+        return KEnum.O.name() + KEnum.O.getValue();
+    }
+}
+
+
+
+// FILE: KEnum.kt
+@Retention(AnnotationRetention.RUNTIME)
+annotation class A
+
+enum class KEnum(@A val value: Any) {
+    O("K")
+}
+
+fun box(): String {
+    return Foo.test();
+}
--- js/js.tests/test/org/jetbrains/kotlin/js/test/semantics/IrJsCodegenBoxTestGenerated.java
+++ js/js.tests/test/org/jetbrains/kotlin/js/test/semantics/IrJsCodegenBoxTestGenerated.java
@@ -8178,6 +8178,16 @@ public class IrJsCodegenBoxTestGenerated extends AbstractIrJsCodegenBoxTest {
             KotlinTestUtils.assertAllTestsPresentByMetadata(this.getClass(), new File("compiler/testData/codegen/box/enum"), Pattern.compile("^(.+)\\.kt$"), TargetBackend.JS_IR, true);
         }
 
+        @TestMetadata("annotatedParameter.kt")
+        public void testAnnotatedParameter() throws Exception {
+            runTest("compiler/testData/codegen/box/enum/annotatedParameter.kt");
+        }
+
+        @TestMetadata("annotatedParameter2.kt")
+        public void testAnnotatedParameter2() throws Exception {
+            runTest("compiler/testData/codegen/box/enum/annotatedParameter2.kt");
+        }
+
         @TestMetadata("asReturnExpression.kt")
         public void testAsReturnExpression() throws Exception {
             runTest("compiler/testData/codegen/box/enum/asReturnExpression.kt");
