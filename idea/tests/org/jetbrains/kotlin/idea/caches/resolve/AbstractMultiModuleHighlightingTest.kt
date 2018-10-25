c376b963ed205d57ff2c4e082593220f15ead6e3
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2018-10-25 13:04:47 +0300

    Check also drop-box content in multi-module line marker tests
    Related to KT-26957

--- idea/tests/org/jetbrains/kotlin/idea/caches/resolve/AbstractMultiModuleHighlightingTest.kt
+++ idea/tests/org/jetbrains/kotlin/idea/caches/resolve/AbstractMultiModuleHighlightingTest.kt
@@ -16,15 +16,62 @@
 
 package org.jetbrains.kotlin.idea.caches.resolve
 
+import com.intellij.codeInsight.daemon.impl.DaemonCodeAnalyzerImpl
+import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiFile
+import com.intellij.rt.execution.junit.FileComparisonFailure
+import com.intellij.testFramework.ExpectedHighlightingData
+import org.jetbrains.kotlin.idea.codeInsight.AbstractLineMarkersTest
 import org.jetbrains.kotlin.idea.multiplatform.setupMppProjectFromDirStructure
 import org.jetbrains.kotlin.idea.stubs.AbstractMultiHighlightingTest
 import org.jetbrains.kotlin.idea.test.PluginTestCaseBase
 import org.jetbrains.kotlin.idea.test.allJavaFiles
 import org.jetbrains.kotlin.idea.test.allKotlinFiles
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.test.KotlinTestUtils
+import org.jetbrains.kotlin.test.TagsTestDataUtil
 import java.io.File
 
 abstract class AbstractMultiModuleHighlightingTest : AbstractMultiHighlightingTest() {
+
+    protected open fun checkLineMarkersInProject(
+        findFiles: () -> List<PsiFile> = { project.allKotlinFiles().excludeByDirective() }
+    ) {
+        checkFiles(findFiles) {
+            val document = myEditor.document
+            val data = ExpectedHighlightingData(
+                document,
+                /* checkWarnings = */ false,
+                /* checkWeakWarnings = */ false,
+                /* checkInfos = */ false,
+                myFile
+            )
+            setActiveEditor(myEditor)
+            data.init()
+            PsiDocumentManager.getInstance(project).commitAllDocuments()
+            doHighlighting()
+
+            val markers = DaemonCodeAnalyzerImpl.getLineMarkers(getDocument(file), project)
+            val expectedFile = File(file.virtualFile.path)
+
+            try {
+                data.checkLineMarkers(markers, document.text)
+            } catch (error: AssertionError) {
+                try {
+                    val actualTextWithTestData = TagsTestDataUtil.insertInfoTags(markers, true, file.text)
+                    KotlinTestUtils.assertEqualsToFile(expectedFile, actualTextWithTestData)
+                } catch (failure: FileComparisonFailure) {
+                    throw FileComparisonFailure(
+                        error.message + "\n" + failure.message,
+                        failure.expected,
+                        failure.actual,
+                        failure.filePath
+                    )
+                }
+            }
+            AbstractLineMarkersTest.assertNavigationElements(project, myFile as KtFile, markers)
+        }
+    }
 
     protected open fun checkHighlightingInProject(
         findFiles: () -> List<PsiFile> = { project.allKotlinFiles().excludeByDirective() }
